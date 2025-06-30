7994889f27a0c4932a2f48d82e3f3d60afd37669
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-06-30 14:48:36 +0300

    first commit

---
+++ Singer.cs
@@ -0,0 +1,10 @@
+namespace Patikafy
+{
+    public class Singer
+    {
+        public string NameSurname { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Selling { get; set; }
+    }
+}
